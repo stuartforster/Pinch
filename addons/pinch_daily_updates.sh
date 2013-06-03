f54225798e30ca44622a84c44c6428fbb09f1c2e
genebean <me@technicalissues.us>
2013-06-03 12:39:17 -0300

    Fixed YUMSTATUS variable as it was being set to the return code of mktemp instead of yum check-update

--- addons/pinch_daily_updates.sh
+++ addons/pinch_daily_updates.sh
@@ -11,7 +11,6 @@
 
 # Global Settings
 YUMDATA=`mktemp`
-YUMSTATUS="$?"
 
 # Delivery Settings
 # Email can be changed to personal email, e.g. myemail@gmail.com
@@ -19,6 +18,7 @@ EMAIL="root"
 FROM=`hostname`
 
 yum check-update >& ${YUMDATA}
+YUMSTATUS="${?}"
 
 if [ -f /var/lock/subsys/yum ]; then
 
@@ -27,11 +27,11 @@ if [ -f /var/lock/subsys/yum ]; then
 
 	else
 
-		if [[ ${YUMSTATUS} = "1" ]]; then
-			SUBJECT="Updates Available"
-			MESSAGE="We found updates on your system (${HOSTNAME}). You can update your system by running yum-update."
+		if [[ ${YUMSTATUS} -eq "100" ]]; then
+			SUBJECT="Updates Available on ${HOSTNAME}"
+			MESSAGE="${HOSTNAME} has updates available. You can update your system by running yum-update."
 
-		elif [[ ${YUMSTATUS} = "0" ]]; then
+		elif [[ ${YUMSTATUS} -eq "0" ]]; then
 			SUBJECT="No Updates Available"
 			MESSAGE="We didn't find any updates available on your system (${HOSTNAME})."
 
